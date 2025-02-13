874b4e5b4af9b214001d6c7e14f06aaea3079ddf
OlisJ <olisj794@gmail.com>
2025-02-13 17:47:06 +0100

    .

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 function add_theme_scripts(){
     wp_enqueue_style("style", get_template_directory_uri()."/style.css", false, "1.0", "all");
+    wp_enqueue_scripts("main",get_template_directory_uri()."/js/main.cs", array("jquery"), "1.0", true);
 }
 
 add_action("wp_enqueue_scripts" , "add_theme_scripts");
